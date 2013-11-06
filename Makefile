6dfb1a694bc6c8cd85b03775423b7dcb9aac6363
ayekat <takeya@bluewin.ch>
2013-11-06 14:40:24 +0100

    Add WM like code.
    Just for fun: create a windows with [space], and remove a window with
    [d]. If there is no window to be removed, the session is closed.

--- Makefile
+++ Makefile
@@ -1,18 +1,20 @@
 CC=gcc
 LDLIBS=-lX11
 LDFLAGS=
-CFLAGS=-DHOST_$(shell hostname) -Wall -Wpedantic -ansi -g
+CFLAGS=-DHOST_$(shell hostname) -Wall -Wpedantic -std=c99 -g
 SOURCES=$(shell find src -name *.c)
 OBJECTS=$(SOURCES:.c=.o)
 EXECUTABLE=stwm
 
-all: $(SOURCES) $(EXECUTABLE)
+build: $(SOURCES) $(EXECUTABLE)
 
 $(EXECUTABLE): $(OBJECTS)
 	$(CC) $(LDLIBS) $(LDFLAGS) $(OBJECTS) -o $@
 
 .c.o:
 	$(CC) -c $(CFLAGS) $? -o $@
+
+all: build run
 
 clean:
 	rm -f $(OBJECTS)
--- src/stwm.c
+++ src/stwm.c
@@ -1,23 +1,176 @@
 #include <X11/Xlib.h>
+#include <X11/cursorfont.h>
+#include <X11/keysym.h>
 #include <stdio.h>  /* fprintf() */
 #include <stdlib.h> /* NULL, EXIT_FAILURE */
 #include <unistd.h> /* sleep() */
 #include <stdbool.h>
+#include <stdarg.h>
 
-#define WBORDER (2)
-#define HEIGHT (768-(2*WBORDER))
-#define WIDTH (1366-(2*WBORDER))
+/* macros */
+#define WBORDER (1)
+#define die(ARGS) warn(ARGS); exit(EXIT_FAILURE)
+
+/* enums */
+enum { CURSOR_NORMAL, CURSOR_RESIZE, CURSOR_MOVE, CURSOR_LAST };
+
+/* structs */
+typedef struct Client Client;
+struct Client {
+	int x,y,w,h;
+	Client *next;
+	Window win;
+};
+
+/* functions */
+void addClient(void);
+void drawClient(Client *);
+void handleButton(XEvent *);
+void handleKeyPress(XEvent *);
+void removeClient(void);
+void run(void);
+void setup(void);
+void warn(char const *, ...);
+
+/* handler functions, in an array to allow easier access */
+void (*handlers[LASTEvent])(XEvent *e) = {
+	[ButtonPress] = handleButton,
+	[KeyPress] = handleKeyPress
+};
+
+/* variables */
+int cbg, cborder;
+Cursor cursor[CURSOR_LAST]; /* TODO */
+Display *dpy;
+bool running;
+Window root;
+int screen;
+int sw, sh; /* screen dimensions */
+Client *clients;
+int count;
+
+void
+addClient(void)
+{
+	Client *c, *cn;
+
+	/* create client */
+	cn = malloc(sizeof(Client));
+	if (cn == NULL) {
+		warn("could not allocate new client\n");
+		return;
+	}
+
+	/* initialise client */
+	cn->x = 10*count; /* TODO */
+	cn->y = 10*count;
+	++count;
+	cn->w = 200;
+	cn->h = 140;
+	cn->win = XCreateSimpleWindow(dpy, root, cn->x, cn->y, cn->w, cn->h, WBORDER, cborder, cbg);
+	cn->next = NULL;
+
+	/* draw client */
+	drawClient(cn);
+
+	/* integrate client */
+	if (clients == NULL) {
+		clients = cn;
+	} else {
+		for (c = clients; c->next != NULL; c = c->next);
+		c->next = cn;
+	}
+}
+
+void
+drawClient(Client *c)
+{
+	XMapWindow(dpy, c->win);
+}
+
+void
+handleButton(XEvent *e)
+{
+	printf("button pressed\n");
+}
+
+void
+handleKeyPress(XEvent *e)
+{
+	switch (XLookupKeysym(&e->xkey, 0)) {
+		case XK_space:
+			addClient();
+			break;
+		case XK_d:
+			removeClient();
+			break;
+	}
+}
+
+void
+removeClient(void)
+{
+	Client *c;
+	if (clients == NULL) {
+		running = false;
+	} else if (clients->next == NULL) {
+		XDestroyWindow(dpy, clients->win);
+	} else {
+		for (c = clients; c->next->next != NULL; c = c->next);
+		XDestroyWindow(dpy, c->next->win);
+		free(c->next);
+		c->next = false;
+		drawClient(c);
+	}
+	--count;
+}
+
+void
+run(void)
+{
+	XEvent e;
+
+	/* event loop */
+	running = true;
+	while (running && !XNextEvent(dpy, &e)) {
+		handlers[e.type](&e);
+	}
+}
+
+void
+setup(void)
+{
+	/* get colours */
+	cbg = BlackPixel(dpy, screen);
+	cborder = WhitePixel(dpy, screen);
+
+	/* get root window */
+	screen = DefaultScreen(dpy);
+	root = RootWindow(dpy, screen);
+
+	/* get screen dimensions */
+	sw = DisplayWidth(dpy, screen);
+	sh = DisplayHeight(dpy, screen);
+
+	/* set mask of input events to handle */
+	XSelectInput(dpy, root, KeyPressMask|ButtonPressMask);
+
+	/* for positioning the windows */
+	count = 0;
+}
+
+void
+warn(char const *format, ...)
+{
+	va_list args;
+	va_start(args, format);
+	vfprintf(stderr, format, args);
+	va_end(args);
+}
 
 int
 main(int argc, char **argv)
 {
-	Display *dpy;
-	Window w, root;
-	int cblack, cwhite;
-	XEvent e;
-	GC gc;
-	bool running = true;
-
 	/* open the display */
 	dpy = XOpenDisplay(NULL);
 	if (dpy == NULL) {
@@ -25,41 +178,8 @@ main(int argc, char **argv)
 		exit(EXIT_FAILURE);
 	}
 
-	/* get some colours */
-	cblack = BlackPixel(dpy, DefaultScreen(dpy));
-	cwhite = WhitePixel(dpy, DefaultScreen(dpy));
-
-	/* get root window */
-	root = DefaultRootWindow(dpy);
-
-	/* create a window (..., ..., top left corner, dimensions, border width&colour, bg colour) */
-	w = XCreateSimpleWindow(dpy, root, 0, 0, WIDTH, HEIGHT, WBORDER, cwhite, cblack);
-
-	/* create a graphics context */
-	gc = XCreateGC(dpy, w, 0, NULL);
-
-	/* map window to display (make it appear); then event loop */
-	XMapWindow(dpy, w);
-	XSelectInput(dpy, w, ExposureMask|KeyPressMask|ButtonPressMask);
-	while (running) {
-		XNextEvent(dpy, &e);
-		switch (e.type) {
-			case Expose:
-				printf("Expose\n");
-				/* draw a line, then flush it */
-				XSetForeground(dpy, gc, cwhite);
-				XDrawLine(dpy, w, gc, 0, 0, 300, 200);
-				XFlush(dpy);
-				break;
-			case KeyPress:
-				printf("KeyPress\n");
-				break;
-			default:
-				printf("default\n");
-				running = false;
-				break;
-		}
-	}
+	setup();
+	run();
 
 	/* close window */
 	XCloseDisplay(dpy);
