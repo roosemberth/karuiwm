50a7621d5bed990e420dd3e5e987cf0abafbd4ff
ayekat <takeya@bluewin.ch>
2013-11-19 00:08:25 +0100

    Add custom startup and shutdown functions.

--- stwm.c
+++ stwm.c
@@ -1001,7 +1001,9 @@ main(int argc, char **argv)
 	stdlog(stdout, "starting ...");
 	init();
 	scan();
+	custom_startup();
 	run();
+	custom_shutdown();
 	cleanup();
 	XCloseDisplay(dpy);
 	stdlog(stdout, "shutting down ...");
